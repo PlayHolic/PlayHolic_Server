1e027769e8115544197a485427c671f46dcc8d12
GAIN LEE <>
2018-05-09 14:43:39 +0900

    [api] 프로젝트 생성

---
+++ src/test/java/com/plic/plicapi/PlicApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.plic.plicapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PlicApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
