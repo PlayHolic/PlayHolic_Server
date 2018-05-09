bb81f5d92ced1c5fd8b8fd513aac350a3425f3bc
GAIN LEE <>
2018-05-09 14:43:39 +0900

    프로젝트 생성

---
+++ src/main/java/com/plic/plicapi/PlicApiApplication.java
@@ -0,0 +1,12 @@
+package com.plic.plicapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlicApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlicApiApplication.class, args);
+	}
+}
